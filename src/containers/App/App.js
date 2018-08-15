2f905c633ecd952229bcd51ce389955b3d817f35
Joe Robinson <drethic@msn.com>
2018-08-15 18:24:07 -0400

    React Testing Tutorial * Code updated to after 'Verify App functionality'

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../../assets/images/logo.svg';
 import './App.css';
 
 class App extends Component {
+  state: {}
+
   render() {
     return (
       <div className="App">
@@ -11,7 +13,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
       </div>
     );
