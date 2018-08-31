51ac647b492cc9738fdc3b4f360914ed2b8da219
Joe Robinson <drethic@msn.com>
2018-08-31 14:35:44 -0400

    Testing Tutorial * Updated App.js with XHR call * Changed App.js render to show the body of each returned post * Updated App.test.js to work with the changes in App.js * Added additional package dependancies

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import _ from 'lodash';
 import logo from '../../assets/images/logo.svg';
 import './App.css';
 
 class App extends Component {
-  state: {}
+  state = {
+    posts: {}
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => this.setState({ posts: res.data }));
+  }
 
   render() {
+    let postContent = null;
+    const { posts } = this.state;
+    if (!_.isEmpty(posts)) {
+      postContent = (
+        <ul>
+          {posts.map(post => <li key={post.id}>{post.body}</li>)}
+        </ul>
+      );
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
+        {postContent}
       </div>
     );
   }
